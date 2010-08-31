97df9dc0ecd0863af38873d01c9baf21e0883176
Joe Edelman <joe.edelman@gmail.com>
2010-08-30 23:01:00 -0700

    go v2

--- go.js
+++ go.js
@@ -1,60 +1,189 @@
-// jspoppyseed
-
 // jq fns used: remove, attr, val, clone, each, is, live
 
-window.App = {};
+if (!window.console) window.console = {};
+if (!window.console.log) console.log = function(x){};
+var This = { changed:{} };
 
-var development_mode = (location.protocol == 'file:');
+// ============
+// = basic go =
+// ============
 
-window.onerror = function (msg, uri, line) {
-  report_error(msg, null, uri + ": " + line);
-  return false; // don't suppress the error
-};
+(function(){
+  var go = function(url) {
+    This.new_url = url;
+    go.trigger('rewrite_url');
+    if (!This.new_url) return;
+    This.url = This.new_url;
+    $.each(This.url.split(';'), function(){
+      var p = this.split('=');
+      go.set(p[0], unescape(p[1]));
+    });
+    go.trigger('url_changed');
+    This.changed = {};
+  };
 
-if (!window.console) {
-  window.console = {};
-  if (!console.log) {
-    console.log = function(x){};
+  var handlers = [];
+  var handler_positions = {};
+  go.NOT_FOUND = {};
+
+  $.extend(go, {
+    sender: function(argses){
+      var args = $.makeArray(argses);
+      var method = args.shift();
+      if (method.indexOf('(') >= 0) { //}){
+        var part = method.split("("); //)
+        var method = part[0];
+        var new_args = eval('([' + part[1].slice(0,-1) + '])');
+        args = new_args.concat(args);
+      }
+      return function(obj){
+        var fn = obj[method];
+        if (!fn) return go.NOT_FOUND;
+        if (go.dev) return fn.apply(obj, args);
+        try { return fn.apply(obj, args); }
+        catch(e) { go.err('error: ', e, "for method " + method + " and args " + args); }
+      };
+    },
+
+    value: function(method, args) {
+      var sender = go.sender(arguments);
+      for(var i=0; i<handlers.length; i++){
+        var result = sender(handlers[i]);
+        if (result !== go.NOT_FOUND) return result;
+      }
+      return go.NOT_FOUND;
+    },
+
+    dispatch: function(method, args) {
+      var result = go.value.apply(go, arguments);
+      if (result === go.NOT_FOUND) return false;
+      else return true;
+    },
+
+    trigger: function(method, args) {
+      var sender = go.sender(arguments);
+      $.each(handlers, function(){ sender(this); });
+    },
+
+    set: function(key, value) {
+      if (This[key] == value) return;
+      This.changed[key] = true;
+      This[key] = value;
+    },
+
+    install: function(name, obj) {
+      handler_positions[name] = (handler_positions[name] || handlers.length);
+      handlers[handler_positions[name]] = obj;
+    },
+
+    push: function(obj) {
+      handlers.push(obj);
+    },
+
+    f: function(where){
+      return function(){ go(where); };
+    },
+
+    err: function(msg, e, place) {
+      This.bugreport = msg + " at " + place;
+      if (e) {
+        console.log(e);
+        This.bugreport += "\nException: " + e;
+        if (window.printStackTrace)
+          This.bugreport += '\nStack trace:\n' + printStackTrace({e:e}).join('\n') + '\n\n';
+      }
+      console.log(This.bugreport);
+      go.trigger('report_error');
+    }
+  });
+
+  window.go = go;
+})();
+
+
+
+// ============================
+// = Some basic go extensions =
+// ============================
+
+go.install('url_handling', {
+  rewrite_url: function() {
+    if (!This.new_url) return;
+    var c1 = This.new_url.charAt(0);
+    if (c1 != '#' && c1 != '@') return;
+    if (c1 == '#') go.dispatch(This.new_url.slice(1));
+    if (c1 == '@') go.dispatch('at_item');
+    This.new_url = null;
+  },
+  url_changed: function() {
+    console.log('go('+This.url+')');
   }
-}
+});
+
+go.install('tool_handler', {
+  url_changed: function() {
+    if (!This.changed.tool) return;
+    $('.' + This.tool + '_tool').activate('tool');
+    go('#tool_unselected');
+    go.install('tool', (App.tools && App.tools[This.tool]) || {});
+    go('#tool_selected');
+  },
+  start: function() {
+    This.changed.tool = true;
+  }
+});
+
+go.install('body_classes', {
+  start: function() {
+    if (!This.dom_ready || This.body_classes_initialized) return;
+    if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+      $('body').addClass('ios');
+    }
+    $("body").bind("ajaxSend", function(){
+      $(this).addClass('refresh');
+    }).bind("ajaxComplete", function(event, req, settings){
+      $(this).removeClass('refresh');
+    });
+    This.body_classes_initialized = true;
+  }
+});
+
 
 // ============
 // = FACEBOOK =
 // ============
 
-window.fbAsyncInit = function() {
-  var had_session = 'maybe';
+(function(){
+  function fbstart() {
+    FB.Event.subscribe('auth.sessionChange', function(response) {
+      if (response.session) $('body').addClass('fb_authed');
+      else $('body').removeClass('fb_authed');
+    });
 
-  FB.Event.subscribe('auth.sessionChange', function(response) {
-    if (response.session) {
-      if (had_session == 'maybe') {
-        App.fb_active_on_startup && App.fb_active_on_startup(response.session.uid);
-      } else if (had_session == 'no') {
-        App.fb_login && App.fb_login(response.session.uid);
-      }
-      $('body').addClass('fb_authed');
-      had_session = 'yes';
-    } else if (had_session == 'yes') {
-      $('body').removeClass('fb_authed');
-      App.fb_logout && App.fb_logout();
-      had_session = 'no';
-    }
-  });
-  
-  FB.init({appId: '31986400134', apiKey: 'cbaf8df3f5953bdea9ce66f77c485c53', status: true, cookie: true, xfbml: true});
-};
+    FB.getLoginStatus(function(response){
+      if (response.session) This.facebook_uid = response.session.uid;
+      go.trigger('facebook_ready');
+    });
 
-function report_error (msg, e, place) {
-  console.log(e);
-  var report = msg + " at " + place;
-  if (e) report += "\nException: " + e;
-  if (window.printStackTrace && e) 
-    report += '\nStack trace:\n' + printStackTrace({e:e}).join('\n') + '\n\n';
-  console.log(report);
-  $.post('/api/bugreport', {issue: report}, function(){
-    App.notify_error && App.notify_error();
-  });
-}
+    FB.Event.subscribe('auth.logout', function(response) {
+      go.dispatch('facebook_logout') || window.location.reload();
+    });
+
+    FB.Event.subscribe('auth.login', function(response) {
+      This.facebook_uid = response.session.uid;
+      go.trigger('facebook_login');
+    });
+  };
+
+  if (!window.FB) window.fbAsyncInit = fbstart;
+  else fbstart();
+})();
+
+
+
+// =====================
+// = jquery extensions =
+// =====================
 
 $.templates = {};
 $.template = function(sel){
@@ -118,9 +247,9 @@ $.fn.enable = function(){
 
 // descends from Klaus Hartl's "cookie plugin"
 $.cookie = function(name, value) {
-  if (typeof value != 'undefined') { 
+  if (typeof value != 'undefined') {
     document.cookie = [name, '=', encodeURIComponent(value)].join('');
-  } else { 
+  } else {
     if (!document.cookie || document.cookie == '') return null;
     var cookies = document.cookie.split('; ');
     for (var i in cookies) {
@@ -133,193 +262,128 @@ $.cookie = function(name, value) {
   }
 };
 
-function dispatch(method, args) {
-  var chain = [].concat(This.first_responders, LiveHTML.widgets, App);
-  var args = $.makeArray(arguments);
-  var method = args.shift();
-  if (method.indexOf('(') >= 0) { //}){
-    var part = method.split("("); //)
-    var method = part[0];
-    var new_args = eval('([' + part[1].slice(0,-1) + '])');
-    args = new_args.concat(args);
+
+// =======================
+// = set up live() calls =
+// =======================
+
+$('a[href],img[href],dl[href],li[href],div[href],h2[href]').live('click', function(){
+  var href = $(this).attr('href');
+  if (!href || href.charAt(0) != "#") {
+    if (this.nodeName == 'A') return true;
+    else return window.location = href;
   }
-  for(var i=0; i<chain.length; i++){
-    if(chain[i][method]) return chain[i][method].guarded_apply(chain[i], args);
-  }
-};
-
-function trigger(method, args) {
-  var chain = [].concat(This.first_responders, LiveHTML.widgets, App);
-  var args = $.makeArray(arguments);
-  var method = args.shift();
-  $.each(chain, function(){
-    if (this[method]) this[method].apply(this, args);
-  });
-};
-
-
-var This = { first_responders: [{}, {}], user: { tag: 'pAnon'} };
-
-$(function(){
-  $('a[href],img[href],dl[href],li[href],div[href],h2[href]').live('click', function(){
-    var href = $(this).attr('href');
-    if (!href || href.charAt(0) != "#") {
-      if (this.nodeName == 'A') return true;
-      else return window.location = href;
-    }
-    if ($(this).is('.toggles.active')) {
-      go('tool=');
-      return false;
-    }
-    go(href.slice(1), null, this);
+  if ($(this).is('.toggles.active')) {
+    go('tool=');
     return false;
-  });
-  
-  $('form').live('submit', function(){
-    var data = $(this).form_values();
-    if (data.error) {
-      alert(data.error);
-      return false;
-    }
-    $(this).disable();
-    var result = dispatch(this.id + "_submitted", data, This, this);
-    if (result != "redo") {
-      $(this).find('input[type=text],textarea').each(function(){ this.value = null; });
-    }
-    $(this).enable();
-    return false;
-  });
-    
-  if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-    $('body').addClass('ios');
   }
-  
-  $("body").bind("ajaxSend", function(){
-    $(this).addClass('refresh');
-  }).bind("ajaxComplete", function(event, req, settings){
-    $(this).removeClass('refresh');
-  });
-  
-  if (window.FB) { window.fbAsyncInit(); }
+  This.clicked = this;
+  go(href.slice(1));
+  return false;
 });
 
-LiveHTML = { widgets: [] };
+$('form').live('submit', function(){
+  This.form_data = $(this).form_values();
+  if (This.form_data.error) {
+    alert(This.form_data.error);
+    return false;
+  }
+  $(this).disable();
+  var result = go.value(this.id + "_submitted", This.form_data, This, this);
+  if (result != "redo") {
+    $(this).find('input[type=text],textarea').each(function(){ this.value = null; });
+  }
+  $(this).enable();
+  return false;
+});
 
-$.fn.app_paint = function(){
-  var data = {};
-  function value_for(method){
-    if (!data[method]) data[method] = This[method] || dispatch(method, This);
-    return data[method] || window[method];
+
+
+// ==============
+// = repainting =
+// ==============
+
+(function(){
+
+  go.push({
+    url_changed: function() {
+      $('.hud:visible, .magic').app_paint();
+    }
+  });
+
+  function scanner(){
+    var data = {};
+    return function(method){
+      var reverse = false;
+      if (method.charAt(0) == '!'){
+        method = method.slice(1);
+        reverse = true;
+      }
+      if (!data[method]) data[method] = This[method] || go.value(method, This);
+      var result = data[method] || window[method];
+      if (reverse) result = !result;
+      return result;
+    };
+  }
+
+  $.fn.app_paint = function(){
+    var value_for = scanner();
+    this.find('[fill]').each(function(){
+      var obj = $(this);
+      var parts = obj.attr('fill').split(' ');
+      var method = parts[0];
+      var attr = parts[1];
+      var value = value_for(method);
+      if (!value) return obj.hide();
+      if (typeof value.valueOf() == 'string') {
+        if (attr) obj.attr(attr, value);
+        else      obj.html(value);
+      } else {
+        console.log("Value ["+value+"] is not an instance of string");
+      }
+    });
+    this.find('[if]').each(function(){
+      var obj = $(this);
+      var method = obj.attr('if');
+      if (value_for(method)) obj.show();
+      else obj.hide();
+    });
+
+    this.find('input.focus').focus();
+
+
+    // TODO: refactor as live() and degrade HTML5 'placeholder' attr
+    this.find('input[hint],textarea[hint]').each(function(){
+      var self = $(this);
+      var hint = self.attr('hint');
+      if (hint.charAt(0) == '#') hint = go.value(hint.slice(1));
+      self.val(hint).addClass('prompting');
+      self.focus(function(){
+        if (self.is('.prompting')) self.val('').removeClass('prompting');
+      });
+      self.blur(function(){ if (!self.val()) self.val(hint).addClass('prompting'); });
+    });
+
+    // TODO: refactor as live()
+    this.find('[observe]').each(function(){
+      var obj = $(this);
+      var methods = obj.attr('observe').split(' METHOD_SPACER ');
+      obj.change(function(){
+        $.each(methods, function(i, method) { go.dispatch(method, obj.val(), null, obj); });
+        return true;
+      });
+      obj.keydown(function(e){
+        var ch = String.fromCharCode(e.which);
+        $.each(methods, function(i, method) { go.dispatch(method, obj.val(), ch, obj); });
+        return true;
+      });
+    });
+
+
+    return this;
   };
-  this.find('[observe]').each(function(){
-    var obj = $(this);
-    var methods = obj.attr('observe').split(' METHOD_SPACER ');
-    obj.change(function(){
-      $.each(methods, function(i, method) { dispatch(method, obj.val(), null, obj); });
-      return true;
-    });
-    obj.keydown(function(e){
-      var ch = String.fromCharCode(e.which);
-      $.each(methods, function(i, method) { dispatch(method, obj.val(), ch, obj); });
-      return true;
-    });
-  });
-  this.find('input[hint],textarea[hint]').each(function(){
-    var self = $(this);
-    var hint = self.attr('hint');
-    if (hint.charAt(0) == '#') hint = dispatch(hint.slice(1));
-    self.val(hint).addClass('prompting');
-    self.focus(function(){ 
-      if (self.is('.prompting')) self.val('').removeClass('prompting'); 
-    });
-    self.blur(function(){ if (!self.val()) self.val(hint).addClass('prompting'); });
-  });
-  this.find('[fill]').each(function(){
-    var obj = $(this);
-    var parts = obj.attr('fill').split(' ');
-    var method = parts[0];
-    var attr = parts[1];
-    var value = value_for(method);
-    if (!value) value = '';
-    if (typeof value.valueOf() == 'string') {
-      if (attr) obj.attr(attr, value);
-      else      obj.html(value);
-    } else {
-      console.log("Value ["+value+"] is not an instance of string");
-    }
-  });
-  this.find('[if]').each(function(){
-    var obj = $(this);
-    var method = obj.attr('if');
-    var reverse = false;
-    if (method.charAt(0) == '!') {
-      method = method.slice(1);
-      reverse = true;
-    }
-    var value = value_for(method);
-    if (reverse) value = !value;
-    if (value) obj.show();
-    else obj.hide();
-  });
-
-  this.find('input.focus').focus();
-  return this;
-};
-
-Function.prototype.guarded_apply = function(obj, args){
-  if (development_mode) return this.apply(obj, args);
-  else try {
-    return this.apply(obj, args);
-  } catch(e) {
-    console.log(args);
-    console.log(e);
-    report_error('error during call: ', e);
-  }
-};
-
-function go(url, form_data, elem) {
-  if (url == '#' || url == '' || url == '@') return;    
-  if (url.charAt(0) == '@') return App.at_link(url);
-  if (url.charAt(0) == '#') {
-    var parts = url.slice(1).split('?');
-    return dispatch(parts[0], unescape(parts[1]), elem);
-  }
-
-  console.log('go('+url+')');
-
-  This.form_data = form_data;
-  var changed = This.changed = {};
-  This.prev_url = This.url;
-  This.url = url;
-  $.each(url.split(';'), function(){
-    var part = this.split('=');
-    set(part[0], unescape(part[1]));
-  });
-
-  if (!This.prev_url) changed.tool = true;
-
-  (function(){
-    if (changed.tool) {
-      $('.' + This.tool + '_tool').activate('tool');
-      go('#tool_unselected');
-      This.first_responders[0] = (App.tools && App.tools[This.tool]) || {};
-      go('#tool_selected');
-    }
-    if (App.update) App.update(This.changed);
-    $('.hud:visible, .magic').app_paint();
-  }).guarded_apply(this);
-
-  App.loaded = true;
-};
+})();
 
 
-function set(key, value) {
-  if (This[key] != value) {
-    This.changed[key] = true;
-    This[key] = value;
-  }
-}
-
-function fgo(what){ 
-  return function(){ go(what); };
-}
+$(function(){ This.dom_ready = true; go.trigger('start'); });
+window.onerror = function (msg, uri, line) { go.err(msg, null, uri + ": " + line); return false; };
