ccaafe4fd085152ba428e5e1549d3d3d8f665060
Noah Miller <noahhmiller@gmail.com>
2010-09-16 14:52:33 -0400

    cookie options and deletion

--- go.js
+++ go.js
@@ -251,9 +251,32 @@ $.fn.enable = function(){
 };
 
 // descends from Klaus Hartl's "cookie plugin"
-$.cookie = function(name, value) {
+// http://plugins.jquery.com/files/jquery.cookie.js.txt
+$.cookie = function(name, value, options) {
   if (typeof value != 'undefined') {
-    document.cookie = [name, '=', encodeURIComponent(value)].join('');
+    options = options || {};
+    if (value === null) {
+        value = '';
+        options.expires = -1;
+    }
+    var expires = '';
+    if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+        var date;
+        if (typeof options.expires == 'number') {
+            date = new Date();
+            date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+        } else {
+            date = options.expires;
+        }
+        expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+    }
+    // CAUTION: Needed to parenthesize options.path and options.domain
+    // in the following expressions, otherwise they evaluate to undefined
+    // in the packed version for some reason...
+    var path = options.path ? '; path=' + (options.path) : '';
+    var domain = options.domain ? '; domain=' + (options.domain) : '';
+    var secure = options.secure ? '; secure' : '';
+    document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
   } else {
     if (!document.cookie || document.cookie == '') return null;
     var cookies = document.cookie.split('; ');
